edcc271405ab7a229488cd0940f9b1be255e2175
Raiicon <orankai@gmail.com>
2019-01-17 00:21:17 -0500

    Working build

---
+++ clicky-game/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Nav from './components/Nav';
+
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Nav />
+          
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ clicky-game/src/components/Nav/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./nav.css";
+
+const Nav = props => (
+    <nav className="navbar-nav">
+        <ul>
+            <li classname="navbar-brand">
+            <a href="/clickyGame/">{/*TO DO */}</a>
+            </li>
+        </ul>
+    </nav>
+)
+
+export default Nav;
