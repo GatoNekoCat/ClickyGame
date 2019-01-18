a0c0839c214158500caf24551256ddf569c1fe46
Raiicon <orankai@gmail.com>
2019-01-18 13:19:12 -0500

    Updates

---
+++ precache-manifest.4b965980ce6ebe6b4edfd8231ed85008.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "f4f9ff3414c2f456b002",
+    "url": "/ClickyGame/static/js/main.f4f9ff34.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/ClickyGame/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "f4f9ff3414c2f456b002",
+    "url": "/ClickyGame/static/css/main.bf1f2a51.chunk.css"
+  },
+  {
+    "revision": "b6ef08145a24bbdae763c394cbdfbe17",
+    "url": "/ClickyGame/index.html"
+  }
+];
